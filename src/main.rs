84c85deb368112bba22dbfb6a7eda91d12c4ae19
trignomtry <northruptrig@gmail.com>
2025-05-27 16:28:00 -0400

    skibidi 3

--- src/main.rs
+++ src/main.rs
@@ -271,7 +271,7 @@ fn main() {
             file_contents.clear();
             for token in tokens {
                 if let Error(_, _) = token.kind {
-                    eprintln!("{} {}", token.kind, token.value);
+                    eprintln!("{}{}", token.kind, token.value);
                 } else if token.kind == String {
                     println!("{} \"{}\" {}", token.kind, token.value, token.value);
                 } else {
